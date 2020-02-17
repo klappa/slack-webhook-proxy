47d6cc4d8a9c0eacf7113f2c582a7cdf08d41bca
Carl Rosell <me@carlrosell.com>
2020-02-17 16:41:01 +0100

    feat: no need to get the channel if the user is already in it

--- lambda/slack-bot.ts
+++ lambda/slack-bot.ts
@@ -8,31 +8,18 @@ const token = process.env.SLACK_TOKEN;
 // Initialize
 const web = new WebClient(token);
 
-export const handler = async (event, context) => {
+export const handler = async (event: any) => {
   const { channel: rawChannel, ...rest } = JSON.parse(event.body);
 
   const channel = rawChannel.replace(/[\s#]/g, '');
-  const groups = await web.groups.list();
-  if (groups.ok && Array.isArray(groups.groups) && groups.groups.length > 0) {
-    const foundChannel = groups.groups.find(a => a.name === channel);
-    if (foundChannel) {
-      await web.chat.postMessage({
-        channel: foundChannel.id,
-        ...rest,
-      });
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: `Message sent to #${channel}`,
-        }),
-      };
-    }
-  }
-
+  await web.chat.postMessage({
+    channel,
+    ...rest,
+  });
   return {
-    statusCode: 404,
+    statusCode: 200,
     body: JSON.stringify({
-      message: `Did not find the channel, please make sure to invite it to your channel!`,
+      message: `Message sent to #${channel}`,
     }),
   };
 };
