217943899d6d235412962e4227b1c040980631a7
Carl Rosell <me@carlrosell.com>
2020-02-19 10:44:48 +0100

    feat: enable cors and better error message for not allowed http methods

--- lambda/post-to-slack.ts
+++ lambda/post-to-slack.ts
@@ -2,16 +2,38 @@ require('dotenv').config();
 
 import { WebClient } from '@slack/web-api';
 
-// Read a token from the environment variables
+// read a token from the environment variables
 const token = process.env.SLACK_TOKEN;
 
-// Initialize
+// initialize slack web client
 const web = new WebClient(token);
 
 export const handler = async (event: any) => {
+  // CORS
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers':
+          'Origin, X-Requested-With, Content-Type, Accept',
+      },
+      body: JSON.stringify({ message: 'You can use CORS' }),
+    };
+  }
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      body: `Method Not Allowed. Only POST is allowed.`,
+    };
+  }
+
   const { channel: rawChannel, ...rest } = JSON.parse(event.body);
 
+  // clean channel name from unwanted characters
   const channel = rawChannel.replace(/[\s#]/g, '');
+
+  // send the message
   await web.chat.postMessage({
     channel,
     ...rest,
